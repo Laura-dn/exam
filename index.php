48c0761277b1a9ec72c59c4bbae65dcfec74c980
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-09-11 09:14:07 +0300

    start

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+//Корневой каталог
+define("ROOT", dirname(__FILE__));
+
+//Подключаем Роутер
+include_once(ROOT . "/components/Router.php");
+
+//Подключение базы данных
+include_once(ROOT . "/components/DataBase.php");
+
+//Вызов Роутера
+$router = new Router();
+$router->run();
+
+?>
