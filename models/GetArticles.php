4c1e6096b56642414eb7297c85aa2f1cd080f479
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-10-29 16:07:05 +0200

    blog

--- models/GetArticles.php
+++ models/GetArticles.php
@@ -6,8 +6,44 @@ class GetArticles
 
     public function getAllArticles()
     {
-        $this->articles = "test";
+        $links = $this->getLinks();
+
+        foreach($links as $value) {
+            $this->articles .= $this->getArticle($value);
+        }
 
         return $this->articles;
+    }
+
+    public function getArticle($data)
+    {
+        $article = '<article class="index_5">
+                        <h3>' . $data["name"] . '</h3>
+                        <img src="/templates/images/imgBlog/' . $data["image"] . '" class="index_6" alt="Blog image - ' . $data["name"] . '">';
+        
+        $text = file(ROOT . "/templates/textFiles/" . $data["text"]);
+
+        foreach($text as $value) {
+            $article .= '<p class="blogText">' . $value . '</p>';
+        }
+
+        return $article . '</article>';
+    }
+
+    public function getLinks()
+    {
+        $cursor = DataBase::getDBConnection();
+
+        $myQuery = "SELECT * FROM `blog`";
+        $arr = $cursor->prepare($myQuery);
+        $arr->execute();
+
+        while($row = $arr->fetch()) {
+            $links[] = $row;
+        }
+
+        $cursor = null;
+
+        return $links;
     }
 }
